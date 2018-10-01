0714daa579ef76b4416001e8a6bb396ed5563355
ilpol <ilya-polozov@mail.ru>
2018-10-01 03:07:13 +0300

    'test_fix'

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,7 +6,10 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = TODO()
+fun max(values: List<Int>): Int = if (values.isNotEmpty()) {
+    var inner_max: Int = values[0]
+    values.forEach { if (it > inner_max) inner_max = it }
+    inner_max } else 0
 
 /**
  * Returns the sum of all `int` values.
@@ -14,4 +17,7 @@ fun max(values: List<Int>): Int = TODO()
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = TODO()
+fun sum(values: List<Int>): Long = if (values.isNotEmpty()) {
+    var inner_sum: Long = 0
+    values.forEach { inner_sum += it }
+    inner_sum } else 0
