346dfe54cf61ff7e3fc62bef98f25f5a245a79c9
ilpol <ilya-polozov@mail.ru>
2018-10-10 01:36:00 +0300

    fixed ktlint

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -1,6 +1,4 @@
 package io.rybalkinsd.kotlinbootcamp.geometry
-import kotlin.math.max
-import kotlin.math.min
 /**
  * Entity that can physically intersect, like flame and player
  */
@@ -11,19 +9,17 @@ interface Collider {
 /**
  * 2D point with integer coordinates
  */
-data class Point(var x: Int,var y: Int) : Collider {
+data class Point(var x: Int, var y: Int) : Collider {
     override fun isColliding(other: Collider): Boolean {
         return when (other) {
             is Point -> {
-                if((x == other.x) && (y == other.y))
+                if ((x == other.x) && (y == other.y))
                     true
                 else
                     false
             }
-
             is Bar -> {
-
-                if(other.firstCornerX <= x && x <= other.secondCornerX && other.firstCornerY <= y && y <= other.secondCornerY)
+                if (other.firstCornerX <= x && x <= other.secondCornerX && other.firstCornerY <= y && y <= other.secondCornerY)
                     true
                 else
                     false
@@ -43,51 +39,42 @@ data class Point(var x: Int,var y: Int) : Collider {
             -> false
         }
     }
-
 }
-
 /**
  * Bar is a rectangle, which borders are parallel to coordinate axis
  * Like selection bar in desktop, this bar is defined by two opposite corners
  * Bar is not oriented
  * (It does not matter, which opposite corners you choose to define bar)
- */
-data class Bar(var firstCornerX: Int,var firstCornerY: Int,var secondCornerX: Int,var secondCornerY: Int) : Collider {
-
-  init{
-      if (firstCornerX > secondCornerX) {
-          var tmp = firstCornerX
-          firstCornerX = secondCornerX
-          secondCornerX = tmp
-      }
-      if (firstCornerY > secondCornerY) {
-          var tmp = firstCornerY
-          firstCornerY = secondCornerY
-          secondCornerY = tmp
-      }
-  }
-
+   */
+data class Bar(var firstCornerX: Int, var firstCornerY: Int, var secondCornerX: Int, var secondCornerY: Int) : Collider {
+    init {
+        if (firstCornerX > secondCornerX) {
+            var tmp = firstCornerX
+            firstCornerX = secondCornerX
+            secondCornerX = tmp
+        }
+        if (firstCornerY > secondCornerY) {
+            var tmp = firstCornerY
+            firstCornerY = secondCornerY
+            secondCornerY = tmp
+        }
+    }
 
     override fun isColliding(other: Collider): Boolean {
         return when (other) {
             is Point -> {
-                if(firstCornerX <= other.x && other.x <= secondCornerX && firstCornerY <= other.y && other.y <= secondCornerY)
+                if (firstCornerX <= other.x && other.x <= secondCornerX && firstCornerY <= other.y && other.y <= secondCornerY)
                     true
                 else
                     false
-
             }
-
             is Bar -> {
-
-                if(firstCornerX>other.secondCornerX || secondCornerX<other.firstCornerX || firstCornerY>other.secondCornerY||secondCornerY<other.firstCornerY)
-                {
-                         false
-                }
+                if (firstCornerX>other.secondCornerX || secondCornerX<other.firstCornerX || firstCornerY>other.secondCornerY || secondCornerY<other.firstCornerY)
+                    false
                 else
                     true
-            }
-            else -> false
+                }
+                else -> false
         }
     }
 }
